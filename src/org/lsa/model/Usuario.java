dfd007b53d1c1a7aefc6b71816e9dafb652c846a
Gregory Jeronimo <gjeronimo-2026116@kinal.edu.gt>
2026-08-27 19:43:38 -0600

    (ft/US-1.3-navegacion-roles) 001 Se agrego Model

---
+++ src/org/lsa/model/Usuario.java
@@ -0,0 +1,23 @@
+package org.lsa.model;
+
+public class Usuario {
+    private String rol;
+    private boolean activo;
+
+    public String getRol() {
+        return rol;
+    }
+
+    public void setRol(String rol) {
+        this.rol = rol;
+    }
+
+    public boolean isActivo() {
+        return activo;
+    }
+
+    public void setActivo(boolean activo) {
+        this.activo = activo;
+    }
+}
+
