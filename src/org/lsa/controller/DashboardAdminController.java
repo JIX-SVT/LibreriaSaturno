486a7c817fb4f9966d5086a5cd2424679b654424
Gregory Jeronimo <orellanabajan@gmail.com>
2026-08-28 10:26:15 -0600

    (ft/US-1.3-navegacion-roles) T1.20 Se agrego Dashboard Admin

--- src/org/lsa/controller/DashboardAdminController.java
+++ src/org/lsa/controller/DashboardAdminController.java
@@ -8,6 +8,8 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.stage.Stage;
+import javafx.scene.control.Alert; 
+import javafx.scene.control.Alert.AlertType;
 import org.lsa.utils.Navegador;
 import org.lsa.utils.SesionUsuario;
 
@@ -15,13 +17,22 @@ public class DashboardAdminController implements Initializable {
 
     @FXML
     public void handleCerrarSesion(ActionEvent event) {
-         SesionUsuario.getInstancia().cerrarSesion();
+        SesionUsuario.getInstancia().cerrarSesion();
         Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         Navegador.cargarVista(stage, "/org/lsa/view/Login.fxml", "Inicio de Sesión");
     }
 
+    @FXML
+    public void handleIngresar(ActionEvent event) {
+        Alert alerta = new Alert(AlertType.WARNING);
+        alerta.setTitle("Sección No Disponible");
+        alerta.setHeaderText(null);
+        alerta.setContentText("Esta sección no está disponible.");
+        alerta.showAndWait();
+    }
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // Inicialización de componentes aquí
     }
-}
+} 
+
