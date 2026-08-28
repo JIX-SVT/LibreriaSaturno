153c2a1e4e0d292c2a04b6305bb53cb1f19f7779
Gabriel Escobedo <gescobedo-2023401@kinal.edu.gt>
2026-08-28 11:58:16 -0600

    (ft/US-1.2-gestion-usuarios) T1.11.5 Se agrego una parte del codigo

--- src/org/lsa/controller/ListaUsuariosController.java
+++ src/org/lsa/controller/ListaUsuariosController.java
@@ -1,26 +1,48 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
- */
+
 package org.lsa.controller;
 
+import java.awt.TextField;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import org.lsa.dao.IMPL.UsuariosDAOIMPL;
+import org.lsa.dao.UsuariosDAO;
+import org.lsa.model.Usuarios;
 
-/**
- * FXML Controller class
- *
- * @author informatica
- */
 public class ListaUsuariosController implements Initializable {
-
-    /**
-     * Initializes the controller class.
-     */
-    @Override
-    public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
     
+    @FXML
+    private TextField txtUsuario;
+    @FXML
+    private TextField txtNombre;
+    @FXML
+    private TextField txtApellido;
+    @FXML
+    private TextField txtCorreo;
+    @FXML
+    private TextField txtContraseña;
+    @FXML
+    private TextField txtconfirmarContraseña;
+    @FXML  
+    
+    @Override 
+    public void initialize(URL url, ResourceBundle rb) {
+    
+
+    }    
+    private final UsuariosDAO usuariosDAO = new UsuariosDAOIMPL();
+    private final ObservableList<Usuarios> listaUsuarios = FXCollections.observableArrayList();
+    
+    @Override
+    public void initialize(URL location, ResourceBundle resources){
+        configurarTabla();
+        cargarTabla();
+        seleccionarFila();
+    }    
+    private void configurarTabla(){
+        
+    }
 }
