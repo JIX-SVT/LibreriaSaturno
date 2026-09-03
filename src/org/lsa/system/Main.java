bcae92900b285a179fb811dee8c5669ed5f334e1
Jafeth Pérez <jperez-2026078@kinal.edu.gt>
2026-09-03 09:18:24 -0600

    feat/ Se corrigio el main quitando la conexion y agregando el metodo cambiarescena

--- src/org/lsa/system/Main.java
+++ src/org/lsa/system/Main.java
@@ -1,41 +1,47 @@
-package org.lsa.system;
-
+package org.rocka.system;
 import java.io.IOException;
-import java.sql.SQLException;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import org.lsa.utils.ConexionSingleton;
 
 public class Main extends Application {
 
+    private static Stage escenarioPrincipal;
+
     @Override
-    public void start(Stage stage) throws SQLException {
-        try {
-            ConexionSingleton miConexion = ConexionSingleton.getInstancia();
-            miConexion.conectar();
-            System.out.println("Bienvenido a Librería Saturno");
-
-
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("/org/lsa/view/Login.fxml"));
-            
-            Parent root = loader.load();
-            Scene scene = new Scene(root);
-
-            stage.setTitle("Librería Saturno");
-            stage.setScene(scene);
-            stage.show();
-
-        } catch (IOException e) {
-            System.err.println("Error al cargar la vista FXML: " + e.getMessage());
-            e.printStackTrace();
-        }
+    public void start(Stage escenarioPrincipal) throws Exception {
+        this.escenarioPrincipal = escenarioPrincipal;
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/org/lsa/view/LoginView.fxml"));
+        Parent root = loader.load();
+        Scene scene = new Scene(root);
+        escenarioPrincipal.setTitle("Rocka Librería");
+        escenarioPrincipal.setScene(scene);
+        escenarioPrincipal.show();
+        Main.escenarioPrincipal = escenarioPrincipal;     
+        cambiarEscena("/org/lsa/view/InicioSesionView.fxml");
     }
 
+    public static void cambiarVista(String fxmlPath) throws Exception {
+        FXMLLoader loader = new FXMLLoader(Main.class.getResource(fxmlPath));
+        Parent root = loader.load();
+        escenarioPrincipal.setScene(new Scene(root));
+    }
+    
+    public static void cambiarEscena(String rutaFXML) throws IOException {
+        //Parent raiz = FXMLLoader.load(getClass().getResource(rutaFXML));
+        Parent raiz = FXMLLoader.load(Main.class.getResource(rutaFXML));                
+        Scene escena = new Scene(raiz); 
+        escenarioPrincipal.setScene(escena);
+        escenarioPrincipal.sizeToScene();
+        escenarioPrincipal.centerOnScreen();
+        escenarioPrincipal.show();        
+    }
+ 
+   
     public static void main(String[] args) {
         launch(args);
     }
-}   
 
+}
