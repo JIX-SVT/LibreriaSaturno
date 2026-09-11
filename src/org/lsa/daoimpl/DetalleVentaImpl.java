da01bd7830825c73c282bb476a6e5d3736391088
Gregory Jeronimo <gjeronimo-2026116@kinal.edu.gt>
2026-09-11 10:09:02 -0600

    Se cambio el impl de venta

--- src/org/lsa/daoimpl/DetalleVentaImpl.java
+++ src/org/lsa/daoimpl/DetalleVentaImpl.java
@@ -1,7 +1,7 @@
 package org.lsa.daoimpl;
 
-import org.lsa.model.DetalleCompra;
-import org.lsa.dao.DetalleCompraDAO;
+import org.lsa.dao.DetalleVentaDAO;
+import org.lsa.model.DetalleVenta;
 import org.lsa.utils.Conexion;
 
 import java.sql.CallableStatement;
@@ -11,94 +11,101 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
-public class DetalleCompraImpl implements DetalleCompraDAO {
+public class DetalleVentaImpl implements DetalleVentaDAO {
 
-    @Override
-    public boolean insertar(DetalleCompra objeto) {
-        String sql = "{call sp_insertardetallecompra(?, ?)}";
+    public boolean insertar(DetalleVenta objeto) {
+        String sql = "{call sp_insertardetalleventa(?, ?, ?, ?, ?)}";
         try (Connection con = Conexion.getInstancia().conectar();
              CallableStatement cs = con.prepareCall(sql)) {
             
-            cs.setInt(1, objeto.getNoCompra());
+            cs.setInt(1, objeto.getNoVenta());
             cs.setString(2, objeto.getIsbn());
+            cs.setInt(3, objeto.getCantidad());
+            cs.setDouble(4, objeto.getPrecioUnitario());
+            cs.setDouble(5, objeto.getSubTotalDetalle());
             
             return cs.executeUpdate() > 0;
         } catch (SQLException e) {
-            System.err.println("Error [Insertar Detalle]: " + e.getMessage());
+            System.err.println("Error [Insertar Detalle Venta]: " + e.getMessage());
             return false;
         }
     }
 
-    @Override
-    public List<DetalleCompra> listar() {
-        List<DetalleCompra> lista = new ArrayList<>();
-        String sql = "{call sp_listardetallecompra()}";
+    public List<DetalleVenta> listar() {
+        List<DetalleVenta> lista = new ArrayList<>();
+        String sql = "{call sp_listardetalleventa()}";
         try (Connection con = Conexion.getInstancia().conectar();
              CallableStatement cs = con.prepareCall(sql);
              ResultSet rs = cs.executeQuery()) {
             
             while (rs.next()) {
-                lista.add(new DetalleCompra(
-                    rs.getInt("id_detalle_compra"),
-                    rs.getInt("no_compra"),
-                    rs.getString("isbn")
+                lista.add(new DetalleVenta(
+                    rs.getInt("id_detalle_venta"),
+                    rs.getInt("id_venta"),
+                    rs.getString("isbn"),
+                    rs.getInt("cantidad"),
+                    rs.getDouble("precio_unitario"),
+                    rs.getDouble("subtotal")
                 ));
             }
         } catch (SQLException e) {
-            System.err.println("Error [Listar Detalle]: " + e.getMessage());
+            System.err.println("Error [Listar Detalle Venta]: " + e.getMessage());
         }
         return lista;
     }
 
-    @Override
-    public DetalleCompra buscar(Integer id) {
-        String sql = "{call sp_buscardetallecompra(?)}";
+    public DetalleVenta buscar(Integer id) {
+        String sql = "{call sp_buscardetalleventa(?)}";
         try (Connection con = Conexion.getInstancia().conectar();
              CallableStatement cs = con.prepareCall(sql)) {
             
             cs.setInt(1, id);
             try (ResultSet rs = cs.executeQuery()) {
                 if (rs.next()) {
-                    return new DetalleCompra(
-                        rs.getInt("id_detalle_compra"),
-                        rs.getInt("no_compra"),
-                        rs.getString("isbn")
+                    return new DetalleVenta(
+                        rs.getInt("id_detalle_venta"),
+                        rs.getInt("id_venta"),
+                        rs.getString("isbn"),
+                        rs.getInt("cantidad"),
+                        rs.getDouble("precio_unitario"),
+                        rs.getDouble("subtotal")
                     );
                 }
             }
         } catch (SQLException e) {
-            System.err.println("Error [Buscar Detalle]: " + e.getMessage());
+            System.err.println("Error [Buscar Detalle Venta]: " + e.getMessage());
         }
         return null;
     }
 
-    @Override
-    public boolean actualizar(DetalleCompra objeto) {
-        String sql = "{call sp_actualizardetallecompra(?, ?, ?)}";
+    public boolean actualizar(DetalleVenta objeto) {
+        String sql = "{call sp_actualizardetalleventa(?, ?, ?, ?, ?, ?)}";
         try (Connection con = Conexion.getInstancia().conectar();
              CallableStatement cs = con.prepareCall(sql)) {
             
-            cs.setInt(1, objeto.getIdDetalleCompra());
-            cs.setInt(2, objeto.getNoCompra());
+            cs.setInt(1, objeto.getIdDetalleventa());
+            cs.setInt(2, objeto.getNoVenta());
             cs.setString(3, objeto.getIsbn());
+            cs.setInt(4, objeto.getCantidad());
+            cs.setDouble(5, objeto.getPrecioUnitario());
+            cs.setDouble(6, objeto.getSubTotalDetalle());
             
             return cs.executeUpdate() > 0;
         } catch (SQLException e) {
-            System.err.println("Error [Actualizar Detalle]: " + e.getMessage());
+            System.err.println("Error [Actualizar Detalle Venta]: " + e.getMessage());
             return false;
         }
     }
 
-    @Override
     public boolean eliminar(Integer id) {
-        String sql = "{call sp_eliminardetallecompra(?)}";
+        String sql = "{call sp_eliminardetalleventa(?)}";
         try (Connection con = Conexion.getInstancia().conectar();
              CallableStatement cs = con.prepareCall(sql)) {
             
             cs.setInt(1, id);
             return cs.executeUpdate() > 0;
         } catch (SQLException e) {
-            System.err.println("Error [Eliminar Detalle]: " + e.getMessage());
+            System.err.println("Error [Eliminar Detalle Venta]: " + e.getMessage());
             return false;
         }
     }
