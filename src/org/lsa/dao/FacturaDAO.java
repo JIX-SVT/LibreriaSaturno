beb7f99b0010794cb1bdda3b5f0f8af29e642e8a
Jafeth Pérez <jperez-2026078@kinal.edu.gt>
2026-09-24 00:34:40 -0600

    feat/Se agrego el apartado de ventas al día utilizando curdate funcion en sql para ver las fechas de las compras siguiendo la misma estrucura de factura

--- src/org/lsa/dao/FacturaDAO.java
+++ src/org/lsa/dao/FacturaDAO.java
@@ -1,9 +1,11 @@
 package org.lsa.dao;
 
-
 import java.util.ArrayList;
 import org.lsa.model.Factura;
 
 public interface FacturaDAO {
     ArrayList<Factura> buscarFactura(int noVenta);
+    
+    // Nuevo método para el resumen del día
+    ArrayList<Factura> obtenerVentasDelDia();
 }
